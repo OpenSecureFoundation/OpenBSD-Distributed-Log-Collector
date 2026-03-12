9ec1c6ad50114a3d0c597a16daf0b6d51b7e2a78
MONGUE YOSSI <46056054+myyrcorp@users.noreply.github.com>
2026-03-12 18:24:48 +0100

    Création du tableau_bord.php

---
+++ interface_web/includes/tableau_bord.php
@@ -0,0 +1,11 @@
+<?php require_once 'includes/connexion_db.php'; ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="assets/style_interface.css">
+    <title>Dashboard DLC</title>
+</head>
+<body>
+    <h1>Administration des Logs</h1>
+    </body>
+</html>
